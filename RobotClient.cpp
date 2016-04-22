58d028beb40130a0574c8d8197be05f2c9ebb2c4
kckoch <kochkyra1@gmail.com>
2016-04-22 08:43:30 -0400

    Update RobotClient.cpp

--- RobotClient.cpp
+++ RobotClient.cpp
@@ -186,16 +186,16 @@ void runCommands(unsigned int polygonSides, unsigned int numCommands) {
 		    strcpy((char*)parser, commands[n]);                            // 3) Insert the command
 		    
 		    //----Sent message will not be encoded
-	        int new_size;
-	        char *encoded = encodeMessage((const char *)messageBody, messageSize, &new_size);
+//	        int new_size;
+//	        char *encoded = encodeMessage((const char *)messageBody, messageSize, &new_size);
 	
-	        if (sendto(sock, encoded, new_size, 0, (struct sockaddr *) &robotServAddr, sizeof(robotServAddr)) != new_size)
-	            DieWithError("\nsendto() sent a different number of bytes than expected\n");
-	        free(encoded);
+//	        if (sendto(sock, encoded, new_size, 0, (struct sockaddr *) &robotServAddr, sizeof(robotServAddr)) != new_size)
+//	            DieWithError("\nsendto() sent a different number of bytes than expected\n");
+//	        free(encoded);
 
 		    // ~~~ The message to the middleware
-//		    if (sendto(sock, messageBody, messageSize, 0, (struct sockaddr *) &robotServAddr, sizeof(robotServAddr)) != messageSize)
-//		        DieWithError("\nsendto() sent a different number of bytes than expected\n");
+		    if (sendto(sock, messageBody, messageSize, 0, (struct sockaddr *) &robotServAddr, sizeof(robotServAddr)) != messageSize)
+		        DieWithError("\nsendto() sent a different number of bytes than expected\n");
 
 		    free(messageBody); // Free the memory of the message, now that it is not longer in use
 
